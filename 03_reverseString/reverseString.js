7caa665d4b23e7ed15907f8cad333bb397955577
cbstorms <storms.christopher.b@gmail.com>
2025-02-03 21:32:38 +0100

    Complete lesson 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+  let letter = "";
+  let string = "";
+  for (let i = phrase.length; i > 0; i--) {
+    letter = phrase[i-1];
+    string += letter;
+  }
+return string;
 
 };
 
